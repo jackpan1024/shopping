9fc736dbe3e69408a18d09c9e7bd8c3f952106cb
jackpan <jackpan7796@qq.com>
2018-11-09 23:18:34 +0800

    完成二级分类列表的信息列表功能

--- letao/public/ad/js/category-second.js
+++ letao/public/ad/js/category-second.js
@@ -1,7 +1,33 @@
 $(function(){
     var page = 1;
     var pageSize = 5;
+    var totalPage = 0;
     getdata(page,pageSize)
+    console.log(totalPage)
+    $('#pagepre').on('click',function(){
+        //    page<totalPage ? page++ : page = totalPage
+        if(page >1){
+            page--
+        }else {
+            page = 1
+            alert('已经第一页了')
+        }
+           
+            getdata(page,pageSize)
+            // console.log(page)
+        })
+     $('#pagenex').on('click',function(){
+        if(page <totalPage){
+            page++
+        }else {
+            page = totalPage
+            alert('已经最后一页了')
+        }
+           
+            // console.log(page)
+            getdata(page,pageSize)
+            // console.log(page)
+     })
     function getdata(page,pageSize){
         $.ajax({
             url:"/category/querySecondCategoryPaging",
@@ -12,6 +38,9 @@ $(function(){
             },
             success:function(res){
                 console.log(res)
+                totalPage = Math.ceil(res.total)
+               var html = template('secondData',res)
+               $('#addList').html(html)
             }
         })
     }
